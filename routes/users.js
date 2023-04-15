929c5564fa3391de987155c9915c1344d809ef64
Rin Kates <erinkates11@gmail.com>
2023-04-15 12:25:04 -0400

    user get and post working

--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,9 @@
 const express = require('express');
-const User = require('../models/users')
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const { getUser, createUser } = require('../controllers/users') //all of our methods inside of controllers/index.js 
 
 
-router.get('/register', (req, res) => {
+router.get('/register', getUser);
+router.post('/register', createUser);
 
-})
-
-router.post('/register', (req, res) => {
-
-    const salt = bcrypt.genSaltSync(10)
-	req.body.password = bcrypt.hashSync(req.body.password, salt)
-
-
-	User.findOne({username: req.body.username}, (err, userExists) => {
-		if (userExists) {
-			res.render('partials/baduser.ejs');
-		} else {
-			User.create(req.body, (err, createdUser) => {
-				// console.log(createdUser)
-				req.session.currentUser = createdUser
-				res.redirect('/products')
-			})
-		}
-	})
-})
-
-
-// 	bcrypt
-//     .hash(request.body.password, 10)
-//     .then((hashedPassword) => {
-//       // create a new user instance and collect the data
-//       const user = new User({
-//         username: request.body.username,
-//         password: hashedPassword,
-//       });
-//       // save the new user
-//       user
-//         .save()
-//         // return success if the new user is added to the database successfully
-//         .then((result) => {
-//           response.status(201).send({
-//             message: "Registration complete",
-//             result,
-//           });
-//         })
-//         // catch error if the new user wasn't added successfully to the database
-//         .catch((error) => {
-//           response.status(500).send({
-//             message: "Registration error",
-//             error,
-//           });
-//         });
-//     })
-// });
-
-// Export the router for use in other modules
 module.exports = router;
